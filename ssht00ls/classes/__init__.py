cc9e641fc32c02a8fc5639d7de61a360971dde0b
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-03 16:21:46 +0100

    Updates
    updates.

--- ssht00ls/classes/__init__.py
+++ ssht00ls/classes/__init__.py
@@ -5,6 +5,7 @@
 # keep this import style so you can also use the keys.Keys() etc directly.
 import syst3m
 from ssht00ls.classes.config import encryption
+from ssht00ls.classes.config import NETWORK_INFO as network_info
 
 # import classes.
 from ssht00ls.classes import utils, installation, sshd, sshfs, scp, agent, keys, connections, smartcards, aliases, ssh, ssync, client, smb
