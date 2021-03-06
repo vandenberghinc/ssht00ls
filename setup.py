89bb4e07bfbed1eb35d8c2ab6f7a1143819aa964
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-06 13:15:17 +0100

    Updates
    updates.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 setup(
 	name='ssht00ls',
-	version='3.19.5',
+	version='3.19.6',
 	description='Some description.',
 	url='http://github.com/vandenberghinc/ssht00ls',
 	author='Daan van den Bergh',
@@ -19,7 +19,7 @@ setup(
             'asgiref>=3.3.1',
             'certifi>=2020.12.5',
             'chardet>=4.0.0',
-            'cl1>=1.12.1',
+            'cl1>=1.12.2',
             'click>=7.1.2',
             'Django>=3.1.6',
             'Flask>=1.1.2',
@@ -36,9 +36,9 @@ setup(
             'sqlparse>=0.4.1',
             'urllib3>=1.26.3',
             'Werkzeug>=1.0.1',
-            'encrypti0n>=3.19.2',
-            'fil3s>=2.13.2',
-            'netw0rk>=1.7.8',
-            'r3sponse>=2.8.9',
-            'syst3m>=2.14.7',
+            'encrypti0n>=3.19.3',
+            'fil3s>=2.13.3',
+            'netw0rk>=1.7.9',
+            'r3sponse>=2.9.0',
+            'syst3m>=2.14.8',
         ],)
