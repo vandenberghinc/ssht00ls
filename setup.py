fc8e43a03d9875680a77fadd5de0d04250d669e2
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-09 20:20:14 +0100

    Updates
    updates.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 setup(
 	name='ssht00ls',
-	version='3.21.9',
+	version='3.22.0',
 	description='Some description.',
 	url='http://github.com/vandenberghinc/ssht00ls',
 	author='Daan van den Bergh',
